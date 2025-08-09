73cb4b8218556ffd954a1c0e032918b6ba88980f
Alisson Miranda <alissonmichelmiranda@gmail.com>
2025-08-09 16:23:32 -0300

    Atividade 22 finalizada

--- 22/meu-script.js
+++ 22/meu-script.js
@@ -18,3 +18,52 @@ btn2.addEventListener("click", () => {
     const sobrenome = prompt('Digite seu sobrenome: ');
     minhaFuncaoArgs(nome, sobrenome);
 });
+
+
+function minhafuncao2(arg){
+    var raiz = Math.sqrt(arg);
+    var msg = 'A raiz quadrada é igual a: ';
+    alert(msg + raiz)
+}
+
+function escreveDiv(texto){
+    document.getElementById("minhaDiv").innerHTML = texto;
+}
+
+const text = document.getElementById("text");
+text.addEventListener("click", () => {
+    escreveDiv("<h1>Programação Web</h1>")
+})
+
+function escreveDiv3() {
+    texto = prompt("Digite um texto qualquerr: ");
+    document.getElementById("minhaDiv2").innerHTML = texto;
+}
+
+const text2 = document.getElementById("text2");
+text2.addEventListener("click", () => {
+    escreveDiv3();
+})
+
+
+const cont = () => {
+    var numero = prompt("Digite um número: ")
+    var n = "";
+    for (let a = 1; a <= numero; a++) {
+        if (a == numero) {
+            n += a + ".";
+        } else {
+            n += a + ", ";
+        }
+    }
+    document.getElementById("contagem").innerHTML = n;
+    document.getElementById("limpar").style.display = "inline-block";
+}
+
+const limpar = () => {
+    document.getElementById("contagem").innerHTML = "";
+    document.getElementById("limpar").style.display = "none";
+}
+
+document.getElementById("contar").addEventListener("click", cont)
+document.getElementById("limpar").addEventListener("click", limpar);
